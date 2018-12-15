df5cbb5756cca0b88ad3be96b5cc63b480cf4723
Steve Ottenad <steveo@bluerooster.com>
2018-12-15 15:43:46 -0800

    first commit

---
+++ src/GuestBook.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+
+class GuestBook extends Component{
+  constructor(props){
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(event){
+    event.preventDefault();
+    const target = event.target;
+    const data = new FormData(event.target);
+    
+
+    fetch('http://localhost:3001/api/signatures', {
+      method: 'POST',
+      headers:{ "Content-Type": "application/json" },
+      body: JSON.stringify({
+        "signature": data.get('guestSignature'),
+        "message": data.get('message')
+      })
+    })
+    .then(res => res.json())
+    .then(function(data){
+      console.log('success')
+      console.log(data);
+      target.reset();
+    })
+    
+
+  }
+
+  render() {
+    return(
+    <div className="guestBook">
+      <form onSubmit={this.handleSubmit}>
+        <label htmlFor="guestSignature">Name</label>
+        <input name="guestSignature" type="text" /><br />
+
+        <label htmlFor="message">Message</label>
+        <input name="message" type="text" /><br />
+        <button> Submit </button>
+      </form>
+    </div>
+    )
+  }
+
+}
+
+export default GuestBook;
